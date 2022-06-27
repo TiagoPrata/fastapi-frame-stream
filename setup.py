1eddb431581924211c034b7475d5a6ff1d8f2eda
Tiago Prata <tiago.prata@br.abb.com>
2022-06-27 16:00:30 -0300

    FIX: fixing setup.py details to upload to PyPi

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import os
 import setuptools
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 install_requires = []
 install_requires.append('imutils')
@@ -11,10 +19,13 @@ setuptools.setup(
     author="Tiago Prata",
     author_email="prataaa@hotmail.com",
     description="Package to easily stream individual frames (MJPEG) using FastAPI",
+    long_description_content_type='text/markdown',
+    long_description=read('README.md'),
+    url = "https://github.com/TiagoPrata/fastapi-frame-stream",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: MIT",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
     ],
